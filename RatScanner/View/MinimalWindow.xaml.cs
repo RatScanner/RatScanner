0fa52f318983b2f1bf6f79170da474acf5033932
Blightbuster <moritz.scheve@gmx.de>
2020-09-27 02:07:09 +0200

    Implement basic minimal ui window

---
+++ RatScanner/View/MinimalWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RatScanner.View
+{
+	/// <summary>
+	/// Interaction logic for MinimalWindow.xaml
+	/// </summary>
+	internal partial class MinimalWindow : Window
+	{
+		internal MinimalWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void OnMouseDown(object sender, MouseButtonEventArgs e)
+		{
+			if (e.ChangedButton == MouseButton.Left) DragMove();
+		}
+	}
+}
