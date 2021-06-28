7dd28352c9df6332ce442e2e8aabb8df04945abf
Thaddeus Bond <thaddeus@thaddeus.io>
2021-06-28 18:15:51 -0400

    NeededItem constructor

--- RatScanner/FetchModels/NeededItem.cs
+++ RatScanner/FetchModels/NeededItem.cs
@@ -5,18 +5,25 @@ namespace RatScanner.FetchModels
 	[Serializable]
 	public class NeededItem
 	{
+
+		// Initialization
+		public NeededItem(string id)
+		{
+			Id = id;
+		}
+
 		// Item data
 		public string Id { get; set; }
 		// The quest number of this item needed by a player according to their progress
-		public int QuestNeeded { get; set; }
+		public int QuestNeeded { get; set; } = 0;
 		// The quest number of this item that this play has towards needed progress
-		public int QuestHave { get; set; }
+		public int QuestHave { get; set; } = 0;
 		// The hideout number of this item needed by a player according to their progress
-		public int HideoutNeeded { get; set; }
+		public int HideoutNeeded { get; set; } = 0;
 		// The hideout number of this item that this play has towards needed progress
-		public int HideoutHave { get; set; }
+		public int HideoutHave { get; set; } = 0;
 		// If any of the quest requirements outstanding are Found in raid required
-		public int FIR { get; set; }
+		public bool FIR { get; set; } = false;
 
 		// Returns total remaining needed
 		public int Remaining
