1b27f2830d1a9923cd2c8057155b1fabdb7d058c
Thaddeus Bond <thaddeus@thaddeus.io>
2021-12-15 12:11:58 -0500

    WebView2 evergreen auto install (#160)

--- RatScanner/App.xaml.cs
+++ RatScanner/App.xaml.cs
@@ -1,5 +1,8 @@
-﻿using SingleInstanceCore;
+﻿using Microsoft.Win32;
+using SingleInstanceCore;
 using System;
+using System.Diagnostics;
+using System.Net;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -10,12 +13,19 @@ namespace RatScanner
 	/// </summary>
 	public partial class App : Application, ISingleInstance
 	{
+		private const string webview2regKey = "HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\Microsoft\\EdgeUpdate\\Clients\\{F3017226-FE2A-4295-8BDF-00C3A9A7E4C5}";
+
 		protected override void OnStartup(StartupEventArgs e)
 		{
 			base.OnStartup(e);
 
 			SetupExceptionHandling();
 
+			// Install webview2 runtime if it is not already
+			var winLogonKey = Registry.GetValue(webview2regKey, "pv", null);
+			if (winLogonKey == null) InstallWebview2Runtime();
+
+			// Setup single instance mode
 			var guid = "{a057bb64-c126-4ef4-a4ed-3037c2e7bc89}";
 			var isFirstInstance = this.InitializeAsFirstInstance(guid);
 			if (!isFirstInstance)
@@ -37,6 +47,34 @@ namespace RatScanner
 				MainWindow.Topmost = !MainWindow.Topmost;
 				MainWindow.Topmost = !MainWindow.Topmost;
 			});
+		}
+
+		private void InstallWebview2Runtime()
+		{
+			using var client = new WebClient();
+			client.DownloadFile("https://go.microsoft.com/fwlink/p/?LinkId=2124703", "MicrosoftEdgeWebview2Setup.exe");
+
+			ProcessStartInfo startInfo = new ProcessStartInfo();
+			startInfo.CreateNoWindow = false;
+			startInfo.UseShellExecute = false;
+			startInfo.FileName = "MicrosoftEdgeWebview2Setup.exe";
+			startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+			startInfo.Arguments = "/silent /install";
+
+			try
+			{
+				// Start the process with the info we specified.
+				// Call WaitForExit and then the using statement will close.
+				Process exeProcess = Process.Start(startInfo);
+				exeProcess.WaitForExit();
+			}
+			catch (Exception ex)
+			{
+				Logger.LogError("Could not install Webview2", ex);
+			}
+
+			try { System.IO.File.Delete("MicrosoftEdgeWebview2Setup.exe"); }
+			catch { }
 		}
 
 		private void SetupExceptionHandling()
